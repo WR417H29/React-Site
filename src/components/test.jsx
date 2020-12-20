91f4138602b8cbf3e7e54db397aff6cafc2976cd
Wraith <i.acnaylor@gmail.com>
2020-12-20 01:00:01 +0000

    Test

---
+++ src/components/test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div class='bg'>
+            <div class='topnav'> 
+                <a class='active' href='#1'> Home </a>
+                <a href='#2'> Page 2 </a>
+            </div>
+        </div>
+    );
+};
+
+export default Test;
