df8ddb52b98182456c453726c7f0bc95b61cdefa
Wraith <i.acnaylor@gmail.com>
2020-12-20 01:07:33 +0000

    Test2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> penis aha </p>
         <Test />
       </header>
     </div>
--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/test.css';
 
 const Test = () => {
     return (
